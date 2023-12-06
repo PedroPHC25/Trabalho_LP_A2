72b954174ce028dc4a3d3f48460e85d4ced9ff25
eduardammag <eduardammag@gmail.com>
2023-12-06 17:30:03 -0300

    c

--- SpaceObjects.py
+++ SpaceObjects.py
@@ -49,6 +49,20 @@ class BigMeteor(SpaceObject):
         if self.x > LARGURA-(20) or self.x < (20):
             self.x_speed = self.x_speed * (-1)
 
+# Classe que herda de SpaceObject e cria o objeto cometa verde
+class GreenComet(SpaceObject):
+    # Redefinição do método abstrato que controla a movimentação
+    def _move(self):
+        if self.y > ALTURA + 100:
+            self.y = randrange(-600, -200)
+            self.x = randrange(50, 550)
+        self.x += self.x_speed 
+        self.y += self.y_speed
+
+        if self.x > LARGURA-(20) or self.x < (20):
+            self.x_speed = self.x_speed * (-1)
+
+
 
 
     
