c314114e285dfbcea9a8310916a92c7507fa3c33
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-12-06 16:59:48 -0300

    Adicionando movimento para o ovni

--- alien.py
+++ alien.py
@@ -4,6 +4,7 @@ from random import randrange
 from screens import LARGURA, ALTURA
 import sprites as spr
 from pygame.locals import QUIT
+from random import randint
 
 class Alien(pygame.sprite.Sprite):
     def __init__(self):
@@ -15,13 +16,31 @@ class Alien(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
-        self.rect.center = (LARGURA//2, ALTURA//2)
+        self.rect.x = -20
+        self.rect.y = 10
+        
+        self.direcao = 0
 
     def update(self):
         if self.index_lista > 24:
             self.index_lista = 0
-        self.index_lista += 0.02
+        self.index_lista += 0.25
         self.image = self.image_ufo[int(self.index_lista)]
+        self.move()
+
+    def move(self):
+        if self.direcao == 0:
+            ufo.rect.y = 10
+            ufo.rect.x += 1
+
+        if self.direcao == 1:
+            ufo.rect.y = 540
+            ufo.rect.x += 1        
+
+        if ufo.rect.x > 2*LARGURA:
+            ufo.rect.x = -80
+            self.direcao = randint(0,1)
+
 
 ufo = Alien()
 todas_sprites = pygame.sprite.Group()
@@ -29,14 +48,16 @@ todas_sprites.add(ufo)
 
 pygame.init()
 tela = pygame.display.set_mode((LARGURA, ALTURA))
+relogio = pygame.time.Clock()
 
 while True:
     tela.fill((0,0,0))
+    relogio.tick(90)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            exit()
+            exit()        
 
     todas_sprites.draw(tela)
     todas_sprites.update()
