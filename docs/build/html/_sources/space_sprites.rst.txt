5b4dfcd10d27eeb9cf7f1df5c09e4dec0b919d47
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-12-06 10:55:52 -0300

    Criei a documentação

---
+++ docs/build/html/_sources/space_sprites.rst.txt
@@ -0,0 +1,9 @@
+Arquivo `space_sprites`
+=======================
+
+.. autoclass:: space_sprites.SpaceSprites
+    :members:
+    :undoc-members:
+    :show-inheritance:
+    :private-members:
+    :special-members: __init__
--- space_sprites.py
+++ space_sprites.py
@@ -2,7 +2,26 @@ import pygame
 
 # Classe das imagens de fundo
 class SpaceSprites(pygame.sprite.Sprite):
+    """
+    Classe referente ao background do jogo.
+
+    :ivar image: Imagem de fundo.
+    :ivar rect: Retângulo utilizado para orientar a imagem.
+    :ivar x: Coordenada x da imagem.
+    :ivar y: Coordenada y da imagem.
+    :ivar speed: Velocidade de movimento da imagem.
+    """
     def __init__(self, x, y, image):
+        """
+        Inicializador do objeto.
+
+        :param x: Coordenada x inicial da imagem.
+        :type x: int
+        :param y: Coordenada y inicial da imagem.
+        :type y: int
+        :param image: Caminho da imagem a ser carregada.
+        :type image: str
+        """
         pygame.sprite.Sprite.__init__(self)
         # Imagens do objeto
         self.image = pygame.image.load(image)
@@ -12,10 +31,14 @@ class SpaceSprites(pygame.sprite.Sprite):
         self.y = y
         self.rect = self.image.get_rect()
         self.rect.topleft = (self.x, self.y)
+        self.speed = 1
 
     # Método para movimentar as imagens de fundo
     def update(self):
-        self.y = self.y + 1
+        """
+        Atualizador da sprite, que se movimenta continuamente para baixo.
+        """
+        self.y = self.y + self.speed
         self.rect.topleft = (self.x, self.y)
         # A imagem volta para cima quando sai da tela por baixo
         if self.y > 600:
