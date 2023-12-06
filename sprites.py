accca4b82ea6a72b6a4a5a0040ddcd8b8b31ec7a
eduardammag <eduardammag@gmail.com>
2023-12-06 18:55:23 -0300

    mudando do cometa verde para fireball

--- sprites.py
+++ sprites.py
@@ -14,22 +14,13 @@ img_ship = pygame.transform.scale(img_ship, (img_ship.get_width()/3, img_ship.ge
 # Carregando a imagem dos tiros
 img_shot = pygame.image.load("images/20.png")
 img_shot = pygame.transform.scale(img_shot, (img_shot.get_width()/5, img_shot.get_height()/5))
-'''
+
 #################################################################################################################################
 
-# Carregando a imagem do cometa verde
-img_green_comet = pygame.image.load("images/copy_green_comet.png")
-img_green_comet_1 = pygame.transform.scale(img_green_comet, (img_green_comet.get_width()/0.5, img_green_comet.get_height()/0.5))
-img_green_comet_2 = pygame.transform.scale(img_green_comet, (img_green_comet.get_width()/0.8, img_green_comet.get_height()/0.8))
-
-# Lista com as imagens do cometa verde
-list_images_comet = [img_green_comet_1, img_green_comet_2]
-
-'''
 paths_images_fireball = []
 
 for image in os.listdir("images/fireball"):
-    paths_images_big_meteor.append(os.path.join("images/fireball", image))
+    paths_images_fireball.append(os.path.join("images/fireball", image))
 
 
 list_images_fireball = []
