c5e4339c8b37afa142a502aa7d8b245a9cf7ffe8
Pedro H. Coterli <pedrocoterli@gmail.com>
2023-12-07 10:29:31 -0300

    Fiz o restart

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from screens import screen, LARGURA, ALTURA, font20
+from screens import screen, LARGURA, ALTURA, font20, font30, formated_text_game_over_1, formated_text_game_over_2
 from player import Ship, Shot
 from sprites import imgs_space, list_images_big_meteor, list_images_fireball
 from SpaceObjects import BigMeteor, Comet
@@ -16,57 +16,51 @@ pygame.mixer.music.play(-1)
 # Relógio para controlar os ticks do jogo
 clock = pygame.time.Clock()
 
-# Grupo com todos os objetos que serão exibidos
-all_sprites = pygame.sprite.Group()
+def game_init():
+    global all_sprites, all_enemies, all_stars, all_player_shots, player_shots_cooldown, shots, game_time, game_screen, ship, player_shots_cooldown, shots
+    # Grupo com todos os objetos que serão exibidos
+    all_sprites = pygame.sprite.Group()
+    # Grupo com todos os inimigos
+    all_enemies = pygame.sprite.Group()
+    # Grupo com todos os tiros do player
+    all_player_shots = pygame.sprite.Group()
+    # Grupo com os sprites das estrelas do fundo
+    all_stars = pygame.sprite.Group()
+    # Adicionando as imagens do fundo nos grupos
+    for each_image in imgs_space:
+        all_sprites.add(each_image)
+        all_stars.add(each_image)
+    # Criando a nave e adicionando-a ao grupo
+    ship = Ship()
+    all_sprites.add(ship)
+    # Variável para controlar o intervalo entre os tiros do player
+    player_shots_cooldown = 50
+    # Lista com todos os tiros
+    shots = []
+    #Criando os meteoros grandes 
+    y = -50
+    for i in range(3):
+        big_meteor= BigMeteor(y, list_images_big_meteor)
+        all_sprites.add(big_meteor)
+        all_enemies.add(big_meteor)
+        y = y - 700
+    #Criando o cometa 
+    y = -50
+    for i in range(2):
+        fireball = Comet(y, list_images_fireball)
+        all_sprites.add(fireball)
+        all_enemies.add(fireball)
+        y = y - 900
+    # Tempo de jogo
+    game_time = 0
+    # Variável para checar se o player perdeu
+    game_screen = "game"
 
-# Grupo com todos os inimigos
-all_enemies = pygame.sprite.Group()
-
-# Grupo com todos os tiros do player
-all_player_shots = pygame.sprite.Group()
-
-# Adicionando as imagens do fundo no grupo
-for each_image in imgs_space:
-    all_sprites.add(each_image)
-
-# Criando a nave e adicionando-a ao grupo
-ship = Ship()
-all_sprites.add(ship)
-
-# Variável para controlar o intervalo entre os tiros do player
-shots_cooldown = 50
-
-# Lista com todos os tiros
-shots = []
-
-#Criando os meteoros grandes 
-y = -50
-for i in range(3):
-    big_meteor= BigMeteor(y, list_images_big_meteor)
-    all_sprites.add(big_meteor)
-    all_enemies.add(big_meteor)
-    y = y - 700
-
-#Criando o cometa 
-y = -50
-for i in range(2):
-    fireball = Comet(y, list_images_fireball)
-    all_sprites.add(fireball)
-    all_enemies.add(fireball)
-    y = y - 900
-
-# Tempo de jogo
-game_time = 0
+game_init()
 
 while True:
-    clock.tick(90)
+    clock.tick(60)
     screen.fill("black")
-    shots_cooldown += 1
-    game_time += 1
-
-    # Texto do tempo de jogo
-    text_time = f"{game_time//10}"
-    formated_text_time = font20.render(text_time, False, "white")
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -74,12 +68,14 @@ while True:
             exit()
         if event.type == KEYDOWN:
             # Criar um novo tiro e adicioná-lo ao grupo de sprites ao apertar a barra de espaço
-            if event.key == K_SPACE and shots_cooldown > 50:
+            if event.key == K_SPACE and player_shots_cooldown > 50:
                 new_shot = Shot(ship.x, ship.y - 40)
                 all_sprites.add(new_shot)
                 all_player_shots.add(new_shot)
-                shots_cooldown = 0
+                player_shots_cooldown = 0
                 player_shot_sound.play()
+            if event.key == K_r and game_screen == "gameover":
+                game_init()
 
     # Movimentação do player por WASD
     if pygame.key.get_pressed()[K_w] and not pygame.key.get_pressed()[K_a] and not pygame.key.get_pressed()[K_d]:
@@ -135,15 +131,30 @@ while True:
 
     # Caso a vida chegue a 0, tela de game over
     if ship.health <= 0:
-        pygame.quit()
-        exit()
+        game_screen = "gameover"
 
-    # Desenhando e atualizando todas as sprites
-    all_sprites.draw(screen)
-    all_sprites.update()
-    screen.blit(formated_text_time, (500, 50))
+    # Texto da pontuação
+    text_time = f"{game_time//10}"
 
-    pygame.draw.rect(screen, "white", (30, 30, 250, 10))
-    pygame.draw.rect(screen, "red", (30, 30, ship.health*50, 10))
+    if game_screen == "game":
+        # Atualiando as variáveis
+        player_shots_cooldown += 1
+        game_time += 1
+        # Desenhando e atualizando todas as sprites
+        all_sprites.draw(screen)
+        all_sprites.update()
+        # Texto do tempo de jogo
+        formated_text_time = font20.render(text_time, False, "white")
+        screen.blit(formated_text_time, (500, 50))
+        # Barra de vida
+        pygame.draw.rect(screen, "white", (30, 30, 250, 10))
+        pygame.draw.rect(screen, "red", (30, 30, ship.health*50, 10))
+    elif game_screen == "gameover":
+        all_stars.draw(screen)
+        all_stars.update()
+        formated_text_time = font30.render(text_time, False, "white")
+        screen.blit(formated_text_game_over_1, (LARGURA/2 - formated_text_game_over_1.get_width()/2, 200))
+        screen.blit(formated_text_time, (LARGURA/2 - formated_text_time.get_width()/2, 285))
+        screen.blit(formated_text_game_over_2, (LARGURA/2 - formated_text_game_over_2.get_width()/2, 350))
 
     pygame.display.flip()
--- screens.py
+++ screens.py
@@ -15,4 +15,13 @@ ALTURA = 600
 screen = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption("Space War")
 
+# Fontes
 font20 = pygame.font.SysFont("arialblack", 20, True, False)
+font40 = pygame.font.SysFont("arialblack", 40, True, False)
+font30 = pygame.font.SysFont("arialblack", 30, True, False)
+
+# Textos da tela de game over
+text_game_over_1 = "Fim de jogo"
+formated_text_game_over_1 = font40.render(text_game_over_1, False, "white")
+text_game_over_2 = "Aperte R para reiniciar"
+formated_text_game_over_2 = font30.render(text_game_over_2, False, "white")
