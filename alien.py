b7d90512a1cfe8955202cf5177f391f0b2459148
Mariana Fernandes <rochamarianafernandes@gmail.com>
2023-12-07 10:40:45 -0300

    Adicionando cometários no código

--- alien.py
+++ alien.py
@@ -1,20 +1,24 @@
 import pygame
 from screens import LARGURA, ALTURA
 import sprites as spr
-from pygame.locals import QUIT, KEYDOWN
+from pygame.locals import QUIT
 from random import randint
 from sounds import ufo_sound
 
-class Alien(pygame.sprite.Sprite):
+# Definindo uma classes para o objeto ovni
+class Ufo(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        # Adicionando a imagem 
         self.image_ufo = spr.list_images_ufo
 
+        # Uma variável para alterna entre as imagens 
         self.index_lista = 0
         self.image = self.image_ufo[self.index_lista]
 
         self.rect = self.image.get_rect()
-
+        
+        # Definindo onde vai ser a primeira geração do objeto
         self.rect.x = - LARGURA
         self.rect.y = 10
         
@@ -22,32 +26,42 @@ class Alien(pygame.sprite.Sprite):
         self.atirar = False
 
     def update(self):
+
+        # Alternando entre as imagens 
         if self.index_lista > 24:
             self.index_lista = 0
         self.index_lista += 0.25
         self.image = self.image_ufo[int(self.index_lista)]
+
+        # Mover a nave pelo eixo x
         self.move()
 
+        # Se a nave  estiver na tela a variável será verdadeira
         if self.rect.x > 10 and self.rect.x < LARGURA:
             self.atirar = True
         else:
             self.atirar = False
 
+        # Antes de passar na tela, o efeito sonoro é ativado
         if self.rect.x == -100:
             ufo_sound.play()
 
     def move(self):
+        # Movimentação pelo eixo x
+        ufo.rect.x += 1
+
+        # Definindo o surgimento abaixo ou acima da tela
         if self.direcao == 0:
             ufo.rect.y = 10
-            ufo.rect.x += 1
 
         if self.direcao == 1:
-            ufo.rect.y = 540
-            ufo.rect.x += 1        
+            ufo.rect.y = 540       
 
+        # Definindo o ciclo de ressurgimento
         if ufo.rect.x > 2*LARGURA:
-            ufo.rect.x = - 3*LARGURA
+            ufo.rect.x = - 2*LARGURA
             self.direcao = randint(0,1)
+            print(self.direcao)
 
 class Laser(pygame.sprite.Sprite):
     def __init__(self, alien):
@@ -66,7 +80,7 @@ class Laser(pygame.sprite.Sprite):
     def update(self):
         self.move()
 
-ufo = Alien()
+ufo = Ufo()
 todas_sprites = pygame.sprite.Group()
 todas_sprites.add(ufo)
 
@@ -85,10 +99,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()   
-        # if event.type == KEYDOWN:
-            # Criar um novo tiro e adicioná-lo ao grupo de sprites ao apertar a barra de espaço
-    if ufo.atirar == True and shots_cooldown > 100:
-        # Criar um novo tiro e adicioná-lo ao grupo de sprites ao apertar a barra de espaço:
+    if ufo.atirar == True and shots_cooldown > 150:
         new_shot = Laser(ufo)
         todas_sprites.add(new_shot)
         all_player_shots.add(new_shot) 
